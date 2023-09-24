257e246ff9498665a155da1416c9673986255c55
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-09-24 12:52:38 +0530

    public redirect

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -77,7 +77,7 @@ const SingleProduct = () => {
 
               <div className="product-warranty-data">
                 <TbTruckDelivery className="warranty-icon" />
-                <p>Thapa Delivered </p>
+                <p> Delivered </p>
               </div>
 
               <div className="product-warranty-data">

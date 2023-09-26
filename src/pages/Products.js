b896af1cc670ea5f726131c28318b2327ee2fb7c
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-09-26 21:13:06 +0530

    remove unused useeffect

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
 import FilterSection from "../components/FilterSection";
 import ProductList from "../components/ProductList";
